95f253eed5129b0d2aa0de2ae49ac11acb1222ce
Nokilos996 <115506050+Nokilos996@users.noreply.github.com>
2026-09-24 02:27:27 +0300

    feat: export canonical finance projection

--- src/core/index.js
+++ src/core/index.js
@@ -3,6 +3,7 @@ export * from './geo.js';
 export * from './format.js';
 export * from './tariff.js';
 export * from './economics.js';
+export * from './legacy-finance.js';
 export * from './vehicle.js';
 export * from './sla.js';
 export * from './track.js';
